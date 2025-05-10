3791be899f2644049d7afa60d70683cdb71d0cb0
Paulo Bonifácio <paulo.bonifacio@live.com>
2025-05-10 09:56:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,18 +7,27 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger/swaggerSpec');
 
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// API Routes
 app.use('/api/contacts', contactRoutes);
+
+// Documentation Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+app.get('/', (req, res) => {
+  res.send('🚀 Contacts API is on! Please visit <a href="/api-docs">/api-docs</a> to get the documentation.');
+});
+
+// Server start
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-{
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
