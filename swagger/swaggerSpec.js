21672afa0e62de30ed661cc94d79d564b8d52231
Paulo Bonifácio <paulo.bonifacio@live.com>
2025-05-10 10:01:29 +0200

    Update swaggerSpec.js

--- swagger/swaggerSpec.js
+++ swagger/swaggerSpec.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: 'https://cse-341-project1-865m.onrender.com',
         description: 'Local server'
       }
     ]
